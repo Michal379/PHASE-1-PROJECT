97e0ceaa9087693aef5d9d164972952a92b79811
Michal <khakaimic4@gmail.com>
2023-04-07 15:53:10 +0300

    card update function

--- index.js
+++ index.js
@@ -5,3 +5,24 @@ function displayDestination(){
     .then(resp=>resp.json())
     .then(favDestinations)
 }
+
+function favDestinations(destinations){
+    destinations.forEach(myDestinations)
+}
+
+function myDestinations(destinations){
+    const dispDestination=document.querySelector(".card")
+    const div=document.createElement("div")
+    div.innerHTML=`
+    <article class="card-body">
+    <div class="card-image">
+      <img src="${destinations.image}" alt="">
+    </div>
+    <div class="card-content">
+     <h4>${destinations.Location}</h4>
+    </div>
+    </article>
+    `
+    dispDestination.appendChild(div)
+
+}

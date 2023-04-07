96821ea9bfdc2cb6c50bb18dbdd8c0dbeac59551
Michal <khakaimic4@gmail.com>
2023-04-07 15:57:35 +0300

    form details

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-
+document.addEventListener("DOMContentLoaded", ()=>{
+    displayDestination()
+})
 
 function displayDestination(){
     fetch("http://localhost:3000/destinations")
@@ -24,5 +26,33 @@ function myDestinations(destinations){
     </article>
     `
     dispDestination.appendChild(div)
-
 }
+
+const form=document.querySelector("#bookings-form").addEventListener( "submit", (e)=>{
+    e.preventDefault()
+
+const firstName=document.getElementById("fname").value
+const lastName=document.getElementById("lname").value
+const email=document.getElementById("email").value
+const date=document.getElementById("date").value
+const availableDestination=document.getElementById("destination").value
+const paymentForm=document.getElementById("pform").value
+const feedback=document.getElementById("feedback").value
+fetch("http://localhost:3000/travelers",{
+    method:"POST",
+    headers:{
+        "Content-Type":"application/json"
+    },
+    body: JSON.stringify({
+        Firstname:firstName,
+        Lastname:lastName,
+        Email:email,
+        Date:date,
+        Destination:availableDestination,
+        Paymentform: paymentForm,
+        feedback:feedback
+    })  
+})
+.then(resp=>resp.json())
+    .then(data=>console.log(data))
+})
